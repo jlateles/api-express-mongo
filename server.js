d2a5dccbebe83d9339508f00782e633bcc730820
jlateles <juliatelesicm@gmail.com>
2025-12-09 16:18:59 -0300

    iniciando o express, deletando rotas do http,etc

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
-import http from "http";
+//import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-    "/": "Curso de Node.js: criando uma API Rest",
-    "/livros": "Testando uma rota para livros",
-    "/autores": "Testando uma rota para autores"
-};
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {"content-type": "text/plain"});
-    res.end(rotas[req.url]);
-});
-
-server.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("servidor escutando!");
 });
