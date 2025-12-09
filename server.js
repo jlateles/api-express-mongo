b7bfd7d9dcae6fca29ced51c3c58fb85ae788282
jlateles <juliatelesicm@gmail.com>
2025-12-09 12:40:45 -0300

    criando rotas e instalando nodemon

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ import http from "http";
 
 const PORT = 3000;
 
+const rotas = {
+    "/": "Curso de Node.js: criando uma API Re"
+};
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"content-type": "text/plain"});
-    res.end("Curso de Node.js: criando uma API Rest");
+    res.end(rotas[req.url]);
 });
 
 server.listen(3000, () => {
