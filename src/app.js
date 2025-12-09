9036cc3bb8cb0e0e4f20251bf54af46caca61182
jlateles <juliatelesicm@gmail.com>
2025-12-09 16:26:30 -0300

    criando rota livros

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 import express from "express";
 const app = express();
 
+const livros = [
+    {
+        id: 1,
+        titulo: "It: A coisa"
+    },
+    {
+        id: 2,
+        titulo: "A hora da estrela"
+    }
+]
+
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js: criando uma API Rest com Express e MongoDB");
+});
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
 });
 
 export default app; 
